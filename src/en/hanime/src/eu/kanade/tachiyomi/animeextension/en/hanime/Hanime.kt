c7c1b093057cc772df50133477c779dbb57704ad
Ala-Gamer <alagamer300@gmail.com>
2025-03-24 18:26:56 +0100

    Update Wit.Anime

--- src/en/hanime/src/eu/kanade/tachiyomi/animeextension/en/hanime/Hanime.kt
+++ src/en/hanime/src/eu/kanade/tachiyomi/animeextension/en/hanime/Hanime.kt
@@ -31,11 +31,11 @@ import java.util.Locale
 
 class Hanime : ConfigurableAnimeSource, AnimeHttpSource() {
 
-    override val name = "hanime.tv"
+    override val name = "WitAnime"
 
-    override val baseUrl = "https://hanime.tv"
+    override val baseUrl = "https://witanime.cyou"
 
-    override val lang = "en"
+    override val lang = "ar"
 
     override val supportsLatest = true
 
